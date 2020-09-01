7b1279523d1282cb16207dfc08089a3b9098bc82
Mateusz Przeliorz - QIAGEN <Mateusz.Przeliorz@qiagen.com>
2020-09-01 17:30:42 +0200

    Project solution created, git pull and clone scripts created

---
+++ scripts/git-clone-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+REPOSITORIES=(Tourtogether Tourtogether.APIGateway)
+
+if [ "$1" = "-p" ]
+  then
+    echo ${REPOSITORIES[@]} | sed -E -e 's/[[:blank:]]+/\n/g' | xargs -I {} -n 1 -P 0 sh -c 'printf "========================================================\nCloning repository: {}\n========================================================\n"; git clone https://github.com/mateusz-przeliorz/{}.git'
+  else
+    for REPOSITORY in ${REPOSITORIES[*]}
+    do
+      echo ========================================================
+      echo Cloning repository: $REPOSITORY
+      echo ========================================================
+      REPO_URL=https://github.com/mateusz-przeliorz/$REPOSITORY.git
+      git clone $REPO_URL
+    done
+fi
